47605f970094c8f57bae5b6c513a9f243b36f5c1
Apollo Daniel <developer.apollo.mail@gmail.com>
2024-01-12 14:08:17 -0300

    Main ui done

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use gtk::prelude::{ApplicationExt, ApplicationExtManual, WidgetExt, ContainerExt, BoxExt};
+
+fn main() {
+    let application = gtk::Application::builder().application_id("org.apollo.power_menu_rs").build();
+
+    const BUTTON_SIZE: i32 = 80;
+    const MARGIN: i32 = 32;
+
+    application.connect_activate(|app|{
+        let window = gtk::ApplicationWindow::builder()
+            .application(app)
+            .title("Power menu")
+            .default_height((BUTTON_SIZE * 3)+(32*4))
+            .default_width(320)
+            .resizable(false)
+            .build();
+
+
+        let container = gtk::Box::new(gtk::Orientation::Vertical, MARGIN,);
+        container.set_margin(MARGIN);
+        container.center_widget();
+
+        let shutdown_button = gtk::Button::with_label("Shutdown");
+        let reboot_button = gtk::Button::with_label("Reboot");
+        let lock_button = gtk::Button::with_label("Lock");
+
+        shutdown_button.set_height_request(BUTTON_SIZE);
+        reboot_button.set_height_request(BUTTON_SIZE);
+        lock_button.set_height_request(BUTTON_SIZE);
+
+        container.add(&shutdown_button);
+        container.add(&reboot_button);
+        container.add(&lock_button);
+
+        window.add(&container);
+        window.show_all();    
+    });
+
+
+
+    application.run();
+}
